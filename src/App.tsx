1685599d422f385f3bd86d8568c9170829078b3c
ganesh kumar <ganesh.kumar@zapcg.com>
2024-03-13 15:21:02 +0530

    routing and top nav bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import './App.css';
 import { useSelector } from 'react-redux';
 import Test from './Pages/Test';
+import ResponsiveAppBar from './Comonents/TopNavBar';
 
 function App() {
   return (
-    <div className="App">
-      <Test />
+    <BrowserRouter>
+    <div>
+    <ResponsiveAppBar />
+    <Routes>
+      <Route path="/test" element={<Test />} />
+    </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
